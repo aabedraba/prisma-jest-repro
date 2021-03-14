73efe7c1291b0b57af5ca706badf5c6994a6a470
Abdallah Abedraba <aabedraba@gmail.com>
2021-03-14 17:03:48 +0100

    Add a first possible solution

--- src/users.test.ts
+++ src/users.test.ts
@@ -1,13 +1,21 @@
-jest.mock("./prisma");
-
 import { getUsers } from "./users";
 import { prisma } from "./prisma";
+jest.mock("./prisma", () => {
+  return {
+    prisma: {
+      user: {
+        findMany: jest.fn(),
+      },
+    },
+  };
+});
 
 const mockedPrismaUsersFindMany = prisma.user.findMany as jest.MockedFunction<
   typeof prisma.user.findMany
 >;
 
 test("get users", async () => {
+  console.log(prisma);
   mockedPrismaUsersFindMany.mockResolvedValueOnce([]);
   const result = await getUsers();
   expect(result).toEqual([]);
